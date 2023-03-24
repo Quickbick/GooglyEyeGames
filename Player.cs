843dd9ed369bab10c111c5f6be59bb31cfb270e1
Nathanael Ostheller <nathanael.ostheller@wsu.edu>
2023-03-24 01:40:41 -0700

    added broken camera

---
+++ Player.cs
@@ -0,0 +1,60 @@
+using Godot;
+using System;
+
+public partial class Player : CharacterBody3D
+{
+	public const float Speed = 5.0f;
+	public const float JumpVelocity = 4.5f;
+
+	// Get the gravity from the project settings to be synced with RigidBody nodes.
+	public float gravity = ProjectSettings.GetSetting("physics/3d/default_gravity").AsSingle();
+	public Node3D neck;
+	public Camera3D camera;
+	
+	public override void _Ready()
+	{
+		base._Ready();
+		neck = GetNode<Node3D>("Neck");
+		camera = GetNode<Camera3D>("Neck/Camera3D");
+	}
+	
+	public override void _Input(InputEvent @event)
+	{
+		if (@event is InputEventMouseMotion mouse){
+			neck.RotateY(-mouse.Relative.X * (float)0.01);
+			camera.RotateX(-mouse.Relative.Y * (float)0.01);
+			//camera.Rotation.X= clamp(camera.rotation.x, ConvertToRadians(-30), ConvertToRadians(60));
+		}
+	}
+
+	public override void _PhysicsProcess(double delta)
+	{
+		Vector3 velocity = Velocity;
+
+		// Add the gravity.
+		if (!IsOnFloor())
+			velocity.Y -= gravity * (float)delta;
+
+		// Handle Jump.
+		if (Input.IsActionJustPressed("jump") && IsOnFloor())
+			velocity.Y = JumpVelocity;
+
+		// Get the input direction and handle the movement/deceleration.
+		// As good practice, you should replace UI actions with custom gameplay actions.
+		Vector2 inputDir = Input.GetVector("move_left", "move_right", "move_forward", "move_back");
+		Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
+		if (direction != Vector3.Zero)
+		{
+			velocity.X = direction.X * Speed;
+			velocity.Z = direction.Z * Speed;
+		}
+		else
+		{
+			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+			velocity.Z = Mathf.MoveToward(Velocity.Z, 0, Speed);
+		}
+
+		Velocity = velocity;
+		MoveAndSlide();
+	}
+}
