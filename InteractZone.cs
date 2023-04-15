cf60521099369acb3d4148342188e974d0fa4d78
Nathanael Ostheller <nathanael.ostheller@wsu.edu>
2023-04-15 13:16:03 -0700

    updated collision to only read interaction layer as layer 2

--- InteractZone.cs
+++ InteractZone.cs
@@ -7,12 +7,28 @@ public partial class InteractZone : Area3D
 	public Sprite2D interactCursor;
 	
 	public override void _Ready(){
-		baseCursor = GetNode<Sprite2D>("./CanvasLayer/BaseCursor");
-		interactCursor = GetNode<Sprite2D>("./CanvasLayer/InteractCursor");
-		baseCursor.Visible = false;
+		baseCursor = GetNode<Sprite2D>("../../../CanvasLayer/Node2D/BaseCursor");
+		interactCursor = GetNode<Sprite2D>("../../../CanvasLayer/Node2D/InteractCursor");
 	}
 	
-	public void OnEnter(Node Other){
-		
+	// When object enters sight sensor
+	private void _on_body_entered(Node3D body)
+	{
+		baseCursor.Visible = false;
+		interactCursor.Visible = true;
+	}	
+	
+	// When object exits sight sensor
+	private void _on_body_exited(Node3D body)
+	{
+		baseCursor.Visible = true;
+		interactCursor.Visible = false;
 	}
+		
 }
+
+
+
+
+
+
